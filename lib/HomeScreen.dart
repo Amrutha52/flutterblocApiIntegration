26e192b4f47accc14a02782da9a28ec680a2597f
Amrutha <16-5581@users.noreply.gitlab.maxvalue.co.in>
2024-09-10 10:06:28 +0530

    HomeScreen converted into stateless

--- lib/HomeScreen.dart
+++ lib/HomeScreen.dart
@@ -3,16 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutterblocapiintegration/products/product_bloc.dart';
 
-class HomeScreen extends StatefulWidget
+class HomeScreen extends StatelessWidget
 {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen>
-{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
